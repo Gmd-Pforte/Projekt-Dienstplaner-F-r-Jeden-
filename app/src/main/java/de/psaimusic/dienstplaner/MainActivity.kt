52e816b9f74a432a8a8ddb1e082f4aa34b089798
PS AI MUSIC Projekte <power.p1986@googlemail.com>
2026-09-14 21:20:34 +0200

    Füge Android Startaktivität hinzu

---
+++ app/src/main/java/de/psaimusic/dienstplaner/MainActivity.kt
@@ -0,0 +1,20 @@
+package de.psaimusic.dienstplaner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import de.psaimusic.dienstplaner.ui.DienstplanApp
+import de.psaimusic.dienstplaner.ui.theme.DienstplanerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        setContent {
+            DienstplanerTheme {
+                DienstplanApp()
+            }
+        }
+    }
+}
