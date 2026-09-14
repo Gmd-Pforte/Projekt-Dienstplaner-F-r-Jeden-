240325fc1b7b3a3a9d22e355fecbe85397db3105
PS AI MUSIC Projekte <power.p1986@googlemail.com>
2026-09-14 22:45:51 +0200

    Aktiviere SchichtPilot 0.4

--- app/src/main/java/de/psaimusic/dienstplaner/MainActivity.kt
+++ app/src/main/java/de/psaimusic/dienstplaner/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import de.psaimusic.dienstplaner.ui.SchichtPilotV3
+import de.psaimusic.dienstplaner.ui.SchichtPilotV4
 import de.psaimusic.dienstplaner.ui.theme.DienstplanerTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
             DienstplanerTheme {
-                SchichtPilotV3()
+                SchichtPilotV4()
             }
         }
     }
